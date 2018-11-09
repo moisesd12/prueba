b58425125800e5af694f8e2c925a01f61c7c1dad
moises <moyses_theboyhotmail.com>
2018-11-08 21:58:38 -0600

    second commit

---
+++ calificaciones.java
@@ -0,0 +1,14 @@
+public class condicion
+{
+    public static void main (String[]args){
+        int cal1 = 35;
+        int cal2 = 90;
+        int calf;
+        calf=(cal1+cal2)/2;
+        if (calf>=70) {
+            System.out.println("aprobado");
+        }
+        else {
+            System.out.println("reprobado");}
+    }
+}

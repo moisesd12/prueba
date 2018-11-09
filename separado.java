801fd025e6184700c5c86d95b45aa76b7af907de
moisesd12 <43596498+moisesd12@users.noreply.github.com>
2018-11-08 22:25:17 -0600

    Add files via upload

---
+++ separado.java
@@ -0,0 +1,23 @@
+public class separado
+{
+    public static void main (String [] args){
+
+        
+    }
+    public static double sumar (double A,double B){
+        double resultado=0;
+        double result=A+B;
+         return result; 
+    }
+    public static double restar (double A,double B){
+        
+        return (A-B);
+    }
+    public static double multiplicar (double A,double B){
+        return (A*B);
+    }
+    public static double modulo  (double A,double B){
+        return (A%B);
+    }
+    
+}
